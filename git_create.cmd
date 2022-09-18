697ef50921ff6768be6e67a7baea26354a6601e5
Fyodor Shevchenko <fyodor.shevchenko@gmail.com>
2022-09-18 23:02:05 +0300

    remove files

--- git_create.cmd
+++
@@ -1,7 +0,0 @@
-echo "# GoIT_HW_7" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/FyodorSh/GoIT_HW_7.git
-git push -u origin main
